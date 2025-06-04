0d1da52e12eb9bdaef8285a6fa44a546c3ff8de1
pavani <pavanirdyv@gmail.com>
2025-06-04 11:04:00 +0530

    added recursive solution

--- Countdigits.cpp
+++ Countdigits.cpp
@@ -8,8 +8,17 @@ int countDigits(long n){
     }
     return count;
 }
+// recursive solution
+int countDigit(long n){
+    if(n == 0){
+        return 0;
+    }else{
+        return 1 + countDigit(n/10);
+    }
+}
 int main(){
     
     cout <<countDigits(3456);
+    cout << countDigit(123);
     return 0;
 }
