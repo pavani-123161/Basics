34bbd2e53e381975a9ccbd30182ee1783a8885af
pavani <pavanirdyv@gmail.com>
2025-06-07 10:48:21 +0530

    kthbit

--- Kthbit.cpp
+++ Kthbit.cpp
@@ -1,14 +1,24 @@
 #include<iostream>
 using namespace std;
+// using left shift operator
 void kthbit(int n,int k){
     if(n & (1 << (k-1)) != 0){
-    cout << yes;
+    cout << "yes" << endl;
     }else{
-        cout << no;
+        cout << "No" << endl;
     }
+}
+// using right shift operator
+void rkthbit(int n, int k){
+    if(n >> (k-1) & 1 == 1){
+    cout << "yes" << endl;
+    }else{
+        cout << "No" <<endl;
+    }  
 
 }
 int main(){
-    Kthbit(5,3);
+     kthbit(5,3);
+     rkthbit(13,3);
     return 0;
 }
